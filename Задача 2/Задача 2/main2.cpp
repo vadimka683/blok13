7cd12e2ee45d1526c3ce9fcbc76d358f8a9472a5
vivasuk683 <vivas@DESKTOP-BE431TS>
2024-11-16 22:23:32 +0300

    Задача 2

---
+++ Задача 2/Задача 2/main2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+float sum(vector<float> price, vector<int> items) {
+	float sum_final = 0;
+	for (int i = 0; i < items.size(); i++) {
+		sum_final += price[items[i]];
+	}
+	return sum_final;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	vector<float> prices{ 2.5, 4.25, 3.0, 10.0 };
+	vector<int> items{ 1, 1, 0, 3 };
+	float sum_final = sum(prices, items);
+	cout <<"Суммарная стоимость будет равна: "<< sum_final;
+}
